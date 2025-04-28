28764640cb202cc64ea7976bd13d810fdde2b55f
Nitika Choudhary <nitikchoudhary1607@gmail.com>
2025-04-28 18:00:32 +0530

    Initial Commit pokemon api project

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setPokemon(data.results);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Pokémon List</h1>
+      <ul>
+        {pokemon.map((poke, index) => (
+          <li key={index}>{poke.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
+export default App
